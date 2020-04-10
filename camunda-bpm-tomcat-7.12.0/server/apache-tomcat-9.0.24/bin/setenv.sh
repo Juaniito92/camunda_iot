0d4f9c3160fde5d0327c84544dec60e2e63fee2e
Juan Rodríguez Dueñas <juaniito.rodriiguez92@gmail.com>
2020-04-10 12:55:52 +0200

    Add camunda-bpm-tomcat

---
+++ camunda-bpm-tomcat-7.12.0/server/apache-tomcat-9.0.24/bin/setenv.sh
@@ -0,0 +1,2 @@
+export CATALINA_OPTS="-Xmx512m -XX:MaxPermSize=256m -XX:PermSize=256m"
+export CLASSPATH=$CLASSPATH:$CATALINA_HOME/lib/mysql-connector-java-8.0.18.jar
---
+++ camunda-bpm-tomcat-7.12.0/shutdown-camunda.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set "CATALINA_HOME=%CD%\server\apache-tomcat-9.0.24"
+
+cd server\apache-tomcat-9.0.24\bin\
+start shutdown.bat
